297e6e781ff10ed0d2dfb0aeac0c774e79d879eb
Stanislav <grobovstax@mail.ru>
2023-02-10 02:27:31 +0600

    Made Base Game Architecture: MonoInstallers GameSession SceneLoader Added PlayerBehaviour

---
+++ Assets/Client/Scripts/Installer/Game/GameInstaller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Zenject;
+
+namespace Client
+{
+    
+}
+public class GameInstaller : MonoInstaller
+{
+    [SerializeField] private PlayerBehaviour _playerBehaviour;
+    public override void InstallBindings()
+    {
+        Container.Bind<PlayerBehaviour>().FromInstance(_playerBehaviour).AsSingle().NonLazy();
+    }
+}
---
+++ Assets/Client/Scripts/SceneLoader/SceneLoader.cs
@@ -0,0 +1,28 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+
+    private void Awake()
+    {
+        _image.DOFade(1, 0f);
+    }
+
+    private async void Start()
+    {
+        await _image.DOFade(0, 0.5f).AsyncWaitForCompletion();
+
+    }
+
+    public async void LoadSceneAsync(string scene)
+    {
+        await _image.DOFade(1, 0.5f).AsyncWaitForCompletion();
+        await SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+        await _image.DOFade(0, 0.5f).AsyncWaitForCompletion();
+    }
+}
