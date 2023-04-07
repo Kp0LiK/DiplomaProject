43d245e6f86ea502779457a2beaaac1f8290bfdc
Stanislav <grobovstax@mail.ru>
2023-04-07 14:21:54 +0600

    Reworked Stamina Controller with run Added DeathState for player Create Base UI Sliders

--- Assets/Client/Scripts/GameCore/NPC/QuestGiver.cs
+++ Assets/Client/Scripts/GameCore/NPC/QuestGiver.cs
@@ -1,31 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestGiver : NPC
+namespace Client
 {
-    [SerializeField] private Quest _quest;
-    [SerializeField] private PlayerBehaviour _playerBehaviour;
+    public class QuestGiver : NPC
+    {
+        [SerializeField] private Quest _quest;
+        [SerializeField] private PlayerBehaviour _playerBehaviour;
     
-    public Quest Quest => _quest;
+        public Quest Quest => _quest;
 
-    protected override void Talk()
-    {
-        base.Talk();
-        GiveQuest();
-    }
+        protected override void Talk()
+        {
+            base.Talk();
+            GiveQuest();
+        }
 
-    private void GiveQuest()
-    {
-        QuestWindow.Activated?.Invoke(_quest.Title, _quest.Description, _quest.ExperienceReward, _quest.GoldReward);
-        QuestWindow.QuestAccepted += AssignQuest;
-    }
+        private void GiveQuest()
+        {
+            QuestWindow.Activated?.Invoke(_quest.Title, _quest.Description, _quest.ExperienceReward, _quest.GoldReward);
+            QuestWindow.QuestAccepted += AssignQuest;
+        }
 
-    private void AssignQuest()
-    {
-        _quest.ActivateQuest();
-        _playerBehaviour.SetActiveQuest(_quest);
-        QuestWindow.QuestAccepted -= AssignQuest;
+        private void AssignQuest()
+        {
+            _quest.ActivateQuest();
+            //_playerBehaviour.SetActiveQuest(_quest);
+            QuestWindow.QuestAccepted -= AssignQuest;
+        }
+
     }
 
 }
--- Assets/Client/Scripts/GameCore/Player/PlayerBehaviour.cs
+++ Assets/Client/Scripts/GameCore/Player/PlayerBehaviour.cs
@@ -2,83 +2,111 @@ using System;
 using Client.Scripts.Data.Player;
 using UnityEngine;
 
-public class PlayerBehaviour : MonoBehaviour
+namespace Client
+{
+    public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private PlayerData _playerData;
-    private NPC _currentNPC;
-    private Quest _currentQuest;
-
-    private Animator _animator;
-    private static readonly int IsAttack = Animator.StringToHash("isAttack");
-    public event Action<int> HealthChanged;
-    
-    private float _speed;
     [SerializeField] private float _gravity;
     [SerializeField] private float _smoothTime;
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private float _groundDistance;
     [SerializeField] private LayerMask _groundMask;
-    
+    [SerializeField] private PlayerAudioData _audioData;
+
+    private Animator _animator;
     private CharacterController _characterController;
+    private AudioSource _audioSource;
 
-    public PlayerData PlayerData => _playerData;
 
+    private float _speed;
+    private float _health;
+    private float _stamina;
     private Vector3 _velocity;
     private bool _isGrounded;
     private float _smooth;
+    private static readonly int IsAttack = Animator.StringToHash("isAttack");
     private static readonly int Speed = Animator.StringToHash("Run");
+    private static readonly int IsDie = Animator.StringToHash("isDie");
 
+    public float Health
+    {
+        get => _health;
+        set
+        {
+            HealthChanged?.Invoke(value);
+            _health = value;
+            if (_health >= _playerData.Health)
+            {
+                _health = _playerData.Health;
+            }
+        }
+    }
+
+    public float Stamina
+    {
+        get => _stamina;
+        set
+        {
+            StaminaChanged?.Invoke(value);
+            _stamina = value;
+            if (_stamina >= _playerData.Stamina)
+            {
+                _stamina = _playerData.Stamina;
+            }
+
+            if (_stamina <= 0)
+            {
+                _stamina = 0;
+                Walk();
+            }
+        }
+    }
+    
+    public event Action<float> HealthChanged;
+    public event Action<float> StaminaChanged;
+
+    public AudioSource AudioSource => _audioSource;
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
         _speed = _playerData.WalkSpeed;
+        _health = _playerData.Health;
+        _stamina = _playerData.Stamina;
     }
 
     private void Update()
     {
-        
-        if (Input.GetKey(KeyCode.E))
+        Debug.Log(_stamina);
+        _animator.SetBool(IsAttack, Input.GetKey(KeyCode.E));
+        if (Input.GetKey(KeyCode.F))
         {
-            _animator.SetBool("isAttack", true);
-        }
-        else
-        {
-            _animator.SetBool("isAttack", false);
+            Health = 0f;
+            _animator.SetBool(IsDie, true);
+            Destroy(gameObject, 5);
         }
 
         Move();
-
-
-        if (Input.GetKeyDown(KeyCode.F) && _currentQuest != null)
-        {
-            if (_currentQuest.IsActive)
-            {
-                _currentQuest.QuestGoal.EnemyKilled();
-                if (_currentQuest.QuestGoal.IsReached())
-                {
-                    _currentQuest.CompleteQuest();
-                }
-            }
-        }
     }
 
     public void ApplyDamage(float damage)
     {
-        if (_playerData.Health <= 0)
+        if (Health <= 0)
         {
-            _playerData.Health = 0;
+            Health = 0;
             _playerData.IsDied = true;
         }
 
         if (_playerData.IsDied)
         {
-            //todo death state
+            _animator.SetBool(IsDie, true);
         }
         else
         {
-            _playerData.Health -= damage;
+            Health -= damage;
+            _audioSource.PlayOneShot(_audioData.OnDetect);
         }
 
         UpdateHealth();
@@ -89,106 +117,94 @@ public class PlayerBehaviour : MonoBehaviour
         HealthChanged?.Invoke(Mathf.RoundToInt(_playerData.Health));
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Move()
     {
-        if (other.gameObject.TryGetComponent(out NPC npc))
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+
+        if (_isGrounded && _velocity.y < 0)
         {
-            npc.Approached?.Invoke(true);
-            _currentNPC = npc;
+            _velocity.y = -2f;
         }
-    }
-    
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.TryGetComponent(out NPC npc))
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var direction = transform.right * horizontal + transform.forward * vertical;
+
+        if (direction != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
         {
-            npc.Approached?.Invoke(false);
-            _currentNPC = null;
+            Walk();
         }
-    }
-
-    public void SetActiveQuest(Quest quest)
-    {
-        _currentQuest = quest;
-    }
-    
-     private void Move()
+        else
         {
-            _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
-
-            if (_isGrounded && _velocity.y < 0)
+            if (direction != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
             {
-                _velocity.y = -2f;
-            }
-
-            var horizontal = Input.GetAxis("Horizontal");
-            var vertical = Input.GetAxis("Vertical");
-
-            var direction = transform.right * horizontal + transform.forward * vertical;
-
-            if (direction != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
-            {
-                Walk();
-            }
-            else
-            {
-                if (direction != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+                Run();
+                if (Stamina >= 60f)
                 {
-                    Run();
+                    Stamina -= 1f;
                 }
                 else
                 {
-                    if (direction == Vector3.zero)
-                    {
-                        Idle();
-                    }
+                    Stamina -= 0.5f;
                 }
             }
-
-            /*if (direction.magnitude >= 0.1f)
+            else
             {
-                var rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-                var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationAngle,
-                    ref _smooth, _smoothTime);
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
-                var move = Quaternion.Euler(0f, rotationAngle, 0f) * Vector3.forward;
-                _characterController.Move(move.normalized * _speed * Time.deltaTime);
-            }*/
-            direction = (Vector3.right * horizontal + Vector3.forward * vertical).normalized;
+                if (direction == Vector3.zero)
+                {
+                    Idle();
+                }
+            }
+        }
 
-            if (direction.magnitude >= 0.1f)
+        /*if (direction.magnitude >= 0.1f)
+        {
+            var rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationAngle,
+                ref _smooth, _smoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            var move = Quaternion.Euler(0f, rotationAngle, 0f) * Vector3.forward;
+            _characterController.Move(move.normalized * _speed * Time.deltaTime);
+        }*/
+        direction = (Vector3.right * horizontal + Vector3.forward * vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            if (Camera.main is { })
             {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +
-                                    Camera.main.transform.eulerAngles.y;
+                var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +
+                                  Camera.main.transform.eulerAngles.y;
                 var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle,
                     ref _smooth, _smoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
-                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 _characterController.Move(moveDir.normalized * _speed * Time.deltaTime);
             }
-
-            _velocity.y += _gravity * Time.deltaTime;
-
-            _characterController.Move(_velocity * Time.deltaTime);
         }
 
-        private void Idle()
-        {
-            _speed = 0f;
-            _animator.SetFloat(Speed, 0f);
-        }
+        _velocity.y += _gravity * Time.deltaTime;
 
-        private void Walk()
-        {
-            _speed = _playerData.WalkSpeed;
-            _animator.SetFloat(Speed, 0.4f);
-        }
+        _characterController.Move(_velocity * Time.deltaTime);
+    }
 
-        private void Run()
-        {
-            _speed = _playerData.RunSpeed;
-            _animator.SetFloat(Speed, 1f);
-        }
+    private void Idle()
+    {
+        _speed = 0f;
+        _animator.SetFloat(Speed, 0f);
+    }
+
+    private void Walk()
+    {
+        _speed = _playerData.WalkSpeed;
+        _animator.SetFloat(Speed, 0.4f);
+    }
+
+    private void Run()
+    {
+        _speed = _playerData.RunSpeed;
+        _animator.SetFloat(Speed, 1f);
+    }
 
     [Serializable]
     public class PlayerAudioData
@@ -201,4 +217,5 @@ public class PlayerBehaviour : MonoBehaviour
         public AudioClip OnCast;
         public AudioClip OnDie;
     }
+}
 }
