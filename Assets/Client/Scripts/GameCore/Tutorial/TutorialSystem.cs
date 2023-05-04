a586f6bc234ef86ea93ec539259293c211bf0be5
Rez-01 <66239707+Rez-01@users.noreply.github.com>
2023-05-04 18:32:02 +0600

    Added Tutorial NPC (code is kinda dirty but it's only for the tutorials so I hope it's ok)

--- Assets/Client/Scripts/GameCore/Tutorial/TutorialSystem.cs
+++ Assets/Client/Scripts/GameCore/Tutorial/TutorialSystem.cs
@@ -6,16 +6,51 @@ using UnityEngine;
 public class TutorialSystem : MonoBehaviour
 {
     [SerializeField] private string[] _greetingLines;
+    [SerializeField] private string[] _interactionLines;
+    [SerializeField] private string[] _postInteractionLines;
+
+    private bool _hasGreeted;
+    private bool _hasInteracted;
+    private bool _hasPostInteracted;
     
     public static Action<string[]> DialogueStarted;
-    private bool _talking;
+    
+    public bool IsExplaining;
+
+    private void OnEnable()
+    {
+        TutorialNPC.OnInteractable += StartInteractionTutorial;
+        DialogueSystem.DialogueEnded += StopExplaining;
+    }
+
+    private void OnDisable()
+    {
+        TutorialNPC.OnInteractable -= StartInteractionTutorial;
+        DialogueSystem.DialogueEnded -= StopExplaining;
+    }
 
     private void Start()
     {
-        if (!_talking)
+        if (!IsExplaining)
         {
             StartCoroutine(BeginTutorial(2f, _greetingLines));
         }
+    }
+
+    private void StartInteractionTutorial()
+    {
+        if (_hasInteracted) return;
+        
+        StartCoroutine(BeginTutorial(1f, _interactionLines));
+        _hasInteracted = true;
+    }
+
+    public void AfterInteraction()
+    {
+        if (_hasPostInteracted) return;
+        
+        StartCoroutine(BeginTutorial(1f, _postInteractionLines));
+        _hasPostInteracted = true;
     }
 
     // Этот метод можно переиспользовать, чтобы включить туториал при надобности
@@ -23,6 +58,11 @@ public class TutorialSystem : MonoBehaviour
     {
         yield return new WaitForSeconds(seconds);
         DialogueStarted?.Invoke(lines);
-        _talking = true;
+        IsExplaining = true;
+    }
+
+    private void StopExplaining()
+    {
+        IsExplaining = false;
     }
 }
